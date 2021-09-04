802521286ef30e6b462c125d6508e431f79c7306
myhkas <mmyhkas@gmail.com>
2021-09-03 19:07:59 -0700

    fixed logout route

--- database/models/usersModel.js
+++ database/models/usersModel.js
@@ -9,7 +9,8 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique:true
+        unique: true,
+        match: [/.+@.+\..+/, "Enter a valid email"],
     },
     passwordHash: {
         type: String,
@@ -26,8 +27,6 @@ const userSchema = new mongoose.Schema({
     //     // required: true,
     //     default: false
     // },
-}, {
-    timestamps: true
 })
 
 const User = mongoose.model('user', userSchema)
--- database/routes/userRoute.js
+++ database/routes/userRoute.js
@@ -1,6 +1,8 @@
 const {getUsers,getUserById, createUser, logInUser, logOutUser} = require("../controllers/userController")
 const router = require("express").Router()
 
+// exporess router method to create route for logging user out
+router.get('/logout', logOutUser)
 
 // express router method to create route for getting all users
 router.route('/').get(getUsers)
@@ -14,7 +16,6 @@ router.route('/create').post(createUser)
 // exporess router method to create route for logging user in
 router.route('/login').post(logInUser)
 
-// exporess router method to create route for logging user out
-router.get('/logout', logOutUser)
+
 
 module.exports = router
